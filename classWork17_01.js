1e3177a130cdd4c66c9c7c2ffceafa421c11aa09
GEVORG KEROBYAN <kerobyangevorg@gmail.com>
2022-02-17 00:39:54 +0400

    feat: assingment 1 completed

---
+++ classWork17_01.js
@@ -0,0 +1,21 @@
+// 1. Write a recursive function to determine whether all digits of the number are odd or not.
+
+let num = 411133;
+
+function allOdd(num) {
+
+   let lastDigit = num % 10;
+   if (num < 10) {
+      return (num % 2 !== 0);
+   }
+   if (lastDigit % 2 == 0) {
+      return false
+   }
+
+   num = (num - lastDigit) / 10;
+   
+   return allOdd(num);
+
+}
+
+console.log(allOdd(num));
