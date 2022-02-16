91f916388a87157cbb3faad1f05a63ea40acb447
GEVORG KEROBYAN <kerobyangevorg@gmail.com>
2022-02-17 01:30:48 +0400

    feat: task 4 completed

---
+++ classWork17_04.js
@@ -0,0 +1,19 @@
+// 4. Given an array. Write a recursive function that removes the first element and returns the
+// given array. (without using arr.shift())
+
+let arr = [6, 78, `n`, 0, 1];
+
+function noShift (arr, i=1, output=[]) {
+
+if (i < arr.length) {
+  output.push(arr[i]);
+  i++;
+  return noShift(arr, i, output);
+}
+return output;
+
+}
+
+console.log(noShift (arr));
+
+// ------------------------------ DONE ------------------------------ \\
